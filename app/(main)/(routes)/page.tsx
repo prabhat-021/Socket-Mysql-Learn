b0249d0eb7ec6a4242063e85f86fe643f23f961d
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-01-08 01:07:38 +0530

    Authentication with clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>this is a Protected Routes</h1>
-    </div>
+    <>
+      <UserButton 
+      afterSignOutUrl="/"/>
+    </>
   )
 }
