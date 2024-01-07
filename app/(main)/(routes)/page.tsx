d10d27b38880081649c28e99a1d12432b4f16a78
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-01-08 01:29:21 +0530

    drak light theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <UserButton 
-      afterSignOutUrl="/"/>
+      <UserButton
+        afterSignOutUrl="/" />
+      <ModeToggle />
     </>
   )
 }
