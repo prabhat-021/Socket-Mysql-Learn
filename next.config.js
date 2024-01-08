30f7800354d8215d4bdf7737f98f25e7ed4746b5
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-01-09 01:54:47 +0530

    Upload image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'utfs.io',
+            pathname: '**',
+          },
+        ],
+      },
+    // images: {
+    //     remotePatterns: [
+    //       "uploadthing.com",
+    //       "utfs.io"
+    //     ]
+    //   }
+}
 
 module.exports = nextConfig
+
+
+
+// In Next.js, the next.config.js file is used to configure various aspects of your Next.js
+//  application, including settings related to images.
+//  It seems like you're trying to configure the images
+//  module to allow loading images from a specific 
+// domain.
+// 
