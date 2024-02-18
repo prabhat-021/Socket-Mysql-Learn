202f088587c163c03b4f85121d8b3a8041d1d1ef
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-02-19 00:56:15 +0530

    Chat Input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import currentProfile from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -39,11 +40,23 @@ export default async function ChannelIdPage({
     }
 
     return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col f-full">
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader
                 serverId={channel.serverId}
                 name={channel.name}
                 type="channel"
+            />
+            <div className="flex-1">
+                Future Messages
+            </div>
+            <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+                channelId:channel.id,
+                serverId:channel.serverId
+            }}
             />
         </div>
     )
