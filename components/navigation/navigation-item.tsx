a783aebe28f3f3ec25aa08f22d6db4b76370ec8c
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-03-07 11:48:57 +0530

    bugs
    routes

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -19,7 +19,7 @@ export default function NavigationItem({
     const router = useRouter();
 
     const onClick = () => {
-        router.push(`/server/${id}`);
+        router.push(`/servers/${id}`);
     }
 
     return (
@@ -30,7 +30,7 @@ export default function NavigationItem({
         >
             <button onClick={onClick} className="group relative flex items-center">
                 <div className={cn("absolute left-0 bg-primary rounded-r-full transition-all w-[4px]", parms?.serverId !== id && "group-hover:h-[20px]", parms?.serverId === id ? "h-[36px]" : "h-[8px]")} />
-                
+
                 <div className={cn(
                     "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
                     parms?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
